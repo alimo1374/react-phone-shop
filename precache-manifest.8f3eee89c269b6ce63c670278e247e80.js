0f631673831535781f0a19517a19d2c5ee210071
m-a-e-z <ali995ez@gmail.com>
2021-01-23 09:58:02 +0330

    Updates

---
+++ precache-manifest.8f3eee89c269b6ce63c670278e247e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0f2cbfd95a0e687483e",
+    "url": "/react-phone-shop/static/css/main.89f8524c.chunk.css"
+  },
+  {
+    "revision": "a0f2cbfd95a0e687483e",
+    "url": "/react-phone-shop/static/js/main.f372e735.chunk.js"
+  },
+  {
+    "revision": "815ebc1c5abee7ff012d",
+    "url": "/react-phone-shop/static/js/runtime~main.9071baf4.js"
+  },
+  {
+    "revision": "428c664f13d3615af7b9",
+    "url": "/react-phone-shop/static/css/2.4b9131c3.chunk.css"
+  },
+  {
+    "revision": "428c664f13d3615af7b9",
+    "url": "/react-phone-shop/static/js/2.250989fa.chunk.js"
+  },
+  {
+    "revision": "88f9b89c392ffca8fe6fca61e665ffe1",
+    "url": "/react-phone-shop/index.html"
+  }
+];
